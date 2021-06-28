97d1ca8d05fefaa9e09f1f0007028bef7793ffa7
hot-meidaimae <webmaster@hot-meidaimae.com>
2021-06-29 02:33:03 +0900

    fix cms locale

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,8 +1,11 @@
 import CMS from "netlify-cms-app";
+import { ja } from 'netlify-cms-locales';
 
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 import IndexPagePreview from "./preview-templates/IndexPagePreview";
 import PricingPagePreview from "./preview-templates/PricingPagePreview";
+
+CMS.registerLocale('ja', ja);
 
 CMS.registerPreviewTemplate("index", IndexPagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
