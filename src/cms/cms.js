03093b9b7367d6e855960fadec421381bcb40880
hot-meidaimae <webmaster@hot-meidaimae.com>
2021-06-29 01:45:16 +0900

    fix cms index to ts

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,13 +1,8 @@
 import CMS from "netlify-cms-app";
-import uploadcare from "netlify-cms-media-library-uploadcare";
-import cloudinary from "netlify-cms-media-library-cloudinary";
 
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 import IndexPagePreview from "./preview-templates/IndexPagePreview";
 import PricingPagePreview from "./preview-templates/PricingPagePreview";
-
-CMS.registerMediaLibrary(uploadcare);
-CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate("index", IndexPagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
--- src/cms/preview-templates/IndexPagePreview.tsx
+++ src/cms/preview-templates/IndexPagePreview.tsx
@@ -1,18 +1,25 @@
 import React from "react";
+import { PreviewTemplateComponentProps } from "netlify-cms-core";
+
 import { IndexPageTemplate } from "../../templates/index-page";
 
-const IndexPagePreview = ({ entry, getAsset }) => {
+const IndexPagePreview: React.FC<PreviewTemplateComponentProps> = ({
+  entry,
+  getAsset,
+}) => {
   const data = entry.getIn(["data"]).toJS();
 
   if (data) {
     return (
       <IndexPageTemplate
-        image={getAsset(data.image)}
-        photos={data.photos.map((el) => getAsset(el.image))}
+        image={getAsset(data.image) as any}
+        photos={data.photos.map((el: any) => getAsset(el.image))}
         heading={data.heading}
         mainpitch={data.mainpitch}
         description={data.description}
         intro={data.intro}
+        newcomerHeading={data.newcomerHeading}
+        newcomerImage={data.newcomerImage}
       />
     );
   } else {
